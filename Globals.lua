abe375ee33c692c63d8e79351a3655993138fe69
Hydra9268 <kxmode1@gmail.com>
2018-05-25 12:11:55 -0700

    Add files via upload

---
+++ Globals.lua
@@ -0,0 +1,11 @@
+local ZGV = ZygorGuidesViewer
+if not ZGV then return end
+
+-----------------------------------------
+-- LOCAL REFERENCES
+-----------------------------------------
+
+EMPTY_STRING = ""
+LEFT_MOUSE_BUTTON = 1
+RIGHT_MOUSE_BUTTON = 2
+MIDDLE_MOUSE_BUTTON = 3
