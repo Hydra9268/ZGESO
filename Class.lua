52e5d2132d4e1898bebad4b74c15b616e0252769
Hydra9268 <kxmode1@gmail.com>
2018-06-09 00:43:54 -0700

    Reformatted a long conditional so that it is easier to read

--- Class.lua
+++ Class.lua
@@ -40,9 +40,20 @@ function Class:New(classType)
 	local class = {
 		class = classType
 	}
-	
+
 	-- zginherits sets __UNSTRICT_CLASS to avoid biting its own tail.
-	if STRICT_CLASSES then setmetatable(class,{__index=function(tab,ind) if not rawget(tab,"__UNSTRICT_CLASS") and type(ind)=="string" and not allowed_nils[classType.."."..ind] and ind:sub(1,1):match("[A-Z]") then error(("|c00cc00%s|r.|c00ff00%s|r not found"):format(classType,ind),2) end end}) end
+	if STRICT_CLASSES then 
+		setmetatable( class, {
+					__index = function(tab,ind) 
+					if not rawget( tab, "__UNSTRICT_CLASS" )
+						and type(ind) == "string" 
+						and not allowed_nils[ classType.."."..ind ] 
+						and ind:sub( 1,1 ):match("[A-Z]") then 
+							error(("|c00cc00%s|r.|c00ff00%s|r not found"):format(classType,ind),2) 
+						end 
+					end
+					}) 
+	end
 
 	zginherits(class,Class) -- Don't use a metatable because might/probably setup a metatable elsewhere
 
