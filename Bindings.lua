f6782b2902420088e2fa67fb551e5a2e90aadcf7
Hydra9268 <kxmode1@gmail.com>
2018-12-20 02:01:11 -0800

    Indent adjustments; Removed unused local variables; Updated global variable;

--- Bindings.lua
+++ Bindings.lua
@@ -1,14 +1,16 @@
-local ZGV = ZGV
+local ZGV = _G.ZGV
 -----------------------------------------
 -- LOCAL REFERENCES
 -----------------------------------------
 
 local L = ZGV.L
+local getGameTimeMS = _G.GetGameTimeMilliseconds
 
 -----------------------------------------
 -- LOCAL FUNCTIONS
 -----------------------------------------
 
+-- What appears in the ESO > CONTROLS > Keybindings window
 function ZGV:RegisterKeyBindings()
     ZO_CreateStringId("SI_BINDING_NAME_ZYGOR_TOGGLE_GUIDE_VIEWER", L["bind_tog"])
     ZO_CreateStringId("SI_BINDING_NAME_ZYGOR_NEXT_STEP", L["bind_next"])
@@ -26,5 +28,5 @@ collectgarbage("collect")
 ZGV.startup_log.load_memory_final = collectgarbage("count")
 ZGV.startup_log.load_memory_total = ZGV.startup_log.load_memory_final - ZGV.startup_log.load_memory_initial
 
-ZGV.startup_log.load_time_final = GetGameTimeMilliseconds() / 1000
+ZGV.startup_log.load_time_final = getGameTimeMS() / 1000
 ZGV.startup_log.load_time_total = ZGV.startup_log.load_time_final - ZGV.startup_log.load_time_initial
