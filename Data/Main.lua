114b704df0dd66495194e1c5cce005e2a11e16f6
Hydra9268 <kxmode1@gmail.com>
2018-12-21 02:00:49 -0800

    Various
    Removed unused local variables;
    Updated global variable;
    Fixed a issue with nil values and number value indexing;
    Indent adjustments;

--- Data/Main.lua
+++ Data/Main.lua
@@ -1,31 +1,18 @@
-local ZGV = ZGV
-if not ZGV then return end
+local ZGV = _G.ZGV
 
 -----------------------------------------
 -- LOCAL REFERENCES
 -----------------------------------------
 
-local tinsert,tremove,sort,min,max,abs,floor,type,pairs,ipairs = table.insert,table.remove,table.sort,math.min,math.max,math.abs,math.floor,type,pairs,ipairs
-local print = ZGV.print
-local CHAIN = ZGV.Utils.ChainCall
-local ui = ZGV.UI
+local tinsert, abs, type, pairs, ipairs = table.insert, math.abs, type, pairs, ipairs
 local svdata -- Set at startup
 
 -----------------------------------------
 -- LOCAL VARIABLES
 -----------------------------------------
 
-local Data = { }
-
-local npcDataCom = ZGV._NpcDataCommon
-local questDataCom = ZGV._QuestDataCommon
-local questStepDataCom = ZGV._QuestStepDataCommon
-local objectDataCom = ZGV._ObjectDataCommon
-local npcData
-local questData
-local questStepData
-local objectData
-
+local npcDataCom, questDataCom, questStepDataCom, objectDataCom = ZGV._NpcDataCommon, ZGV._QuestDataCommon, ZGV._QuestStepDataCommon, ZGV._ObjectDataCommon
+local npcData, questData, questStepData, objectData
 local itemData = ZGV._ItemData
 
 local NPC_STR = "npc"
@@ -34,23 +21,22 @@ local QUEST_STEP_STR = "queststep"
 local OBJECT_STR = "object"
 local ITEM_STR = "item"
 
-
 -- Last id is stored in here so we know what to assign the next one found to. Quest and Npc are indexed by zone id
 local allLastUsedIds = {
-  quest = {},
-  npc = {},
-  object = {},
-  item = 0,
+	quest = {},
+	npc = {},
+	object = {},
+	item = 0,
 }
 
 -----------------------------------------
 -- SAVED REFERENCES
 -----------------------------------------
 
+local Data = {}
 ZGV.Data = Data
 Data.allLastUsedIds = allLastUsedIds
 Data.bloackSavedVars = false
-
 
 -----------------------------------------
 -- LOCAL HELPER FUNCTIONS
@@ -58,40 +44,40 @@ Data.bloackSavedVars = false
 
 -- Returns data,savedvars,lastIds
 local function getCorrectVarsByType(typ)
-  assert(svdata,"svdata not present, did initialization on startup fail?")
-  local comdata,data,saveddata,lastIds
-  if typ == NPC_STR then
-    comdata,data,saveddata,lastIds = npcDataCom,npcData,svdata.npcIds,allLastUsedIds.npc
-  elseif typ == QUEST_STR then
-    comdata,data,saveddata,lastIds = questDataCom,questData,svdata.questIds,allLastUsedIds.quest
-  elseif typ == QUEST_STEP_STR then
-    -- Not string based so not treated exactly the same. Maintenance of data is up to Quest.lua
-    comdata,data,saveddata,lastIds = questStepDataCom,questStepData,ZGV.sv.char.savedquests
-  elseif typ == OBJECT_STR then
-    comdata,data,saveddata,lastIds = objectDataCom,objectData,svdata.objectIds,allLastUsedIds.object
-  elseif typ == ITEM_STR then
-    -- TODO
-    data,saveddata,lastIds = itemData,svdata.itemIds,allLastUsedIds.item
-  end
+	assert(svdata,"svdata not present, did initialization on startup fail?")
+	local comdata,data,saveddata,lastIds
+	if typ == NPC_STR then
+		comdata,data,saveddata,lastIds = npcDataCom,npcData,svdata.npcIds,allLastUsedIds.npc
+	elseif typ == QUEST_STR then
+		comdata,data,saveddata,lastIds = questDataCom,questData,svdata.questIds,allLastUsedIds.quest
+	elseif typ == QUEST_STEP_STR then
+		-- Not string based so not treated exactly the same. Maintenance of data is up to Quest.lua
+		comdata,data,saveddata,lastIds = questStepDataCom,questStepData,ZGV.sv.char.savedquests
+	elseif typ == OBJECT_STR then
+		comdata,data,saveddata,lastIds = objectDataCom,objectData,svdata.objectIds,allLastUsedIds.object
+	elseif typ == ITEM_STR then
+		-- TODO
+		data,saveddata,lastIds = itemData,svdata.itemIds,allLastUsedIds.item
+	end
 
-  if Data.bloackSavedVars then saveddata = "" end -- Sometimes want to block saved variables.
+	if Data.bloackSavedVars then saveddata = "" end -- Sometimes want to block saved variables.
 
-  return comdata,data,saveddata,lastIds
+	return comdata,data,saveddata,lastIds
 end
 
 local function appendSavedVarsByType(typ,entry)
-  if not ZGV.DEV then return end
-  local appendPat = "%s%s\n"
-  if typ == NPC_STR then
-    svdata.npcIds = (appendPat):format(svdata.npcIds or "",entry)
-  elseif typ == QUEST_STR then
-    ZGV.Utils.ShowFloatingMessage(("New Quest Id! %s"):format(entry))
-    svdata.questIds = (appendPat):format(svdata.questIds or "",entry)
-  elseif typ == OBJECT_STR then
-    svdata.objectIds = (appendPat):format(svdata.objectIds or "",entry)
-  elseif typ == ITEM_STR then
-    svdata.itemIds = (appendPat):format(svdata.itemIds or "",entry)
-  end
+	if not ZGV.DEV then return end
+	local appendPat = "%s%s\n"
+	if typ == NPC_STR then
+		svdata.npcIds = (appendPat):format(svdata.npcIds or "",entry)
+	elseif typ == QUEST_STR then
+		ZGV.Utils.ShowFloatingMessage(("New Quest Id! %s"):format(entry))
+		svdata.questIds = (appendPat):format(svdata.questIds or "",entry)
+	elseif typ == OBJECT_STR then
+		svdata.objectIds = (appendPat):format(svdata.objectIds or "",entry)
+	elseif typ == ITEM_STR then
+		svdata.itemIds = (appendPat):format(svdata.itemIds or "",entry)
+	end
 end
 
 -----------------------------------------
@@ -108,129 +94,107 @@ end
 --]]
 
 local function lamehash(s)
-  local sum = 0
-  for i=1,#s do
-    sum = sum + (i*137)*s:byte(i) - 1
-    sum=sum%10000
-  end
-  return sum
+	local sum = 0
+	for i = 1, #s do
+		sum = sum + (i*137)*s:byte(i) - 1
+		sum = sum % 10000
+	end
+	return sum
 end
 
 local function faction_to_num(fac)
-  if not fac then fac=ZGV.Utils.GetFaction() end
-  if fac=="AD" then return 1
-  elseif fac=="DC" then return 2
-  elseif fac=="EP" then return 3
-  else return 0 end
+	if not fac then 
+		fac = ZGV.Utils.GetFaction() 
+	end
+	if fac == "AD" then return 1
+	elseif fac == "DC" then return 2
+	elseif fac == "EP" then return 3
+	else return 0 end
 end
 
 function Data:GetIdByNameAndType(typ,name)
-  --table.insert(ZGV.PRELOG,"GetIdByNameAndType "..typ.." "..name)
-  if not (typ and name) then return end
-  assert(type(name)=="string","Name must be a string")
+	if not (typ and name) then return end
+	assert(type(name)=="string","Name must be a string")
 
-  local comdata,data,saveddata,lastIds = getCorrectVarsByType(typ)
+	local comdata,data,saveddata,lastIds = getCorrectVarsByType(typ)
 
-  local patname = name:gsub("([%.%-%+%?%*%[%]%^%$%%&%(%)'])","%%%1") -- Fixes some possible problems with pattern name. like -
+	local patname = name:gsub("([%.%-%+%?%*%[%]%^%$%%&%(%)'])","%%%1") -- Fixes some possible problems with pattern name. like -
 
-  local p = patname..'=(%-?%d+)\n?'
+	local p = patname..'=(%-?%d+)\n?'
 
-  -- Check in common, then regular, then saved.
-  local id = (comdata and comdata:match(p)) or (data and data:match(p)) or (saveddata and saveddata:match(p))
+	-- Check in common, then regular, then saved.
+	local id = (comdata and comdata:match(p)) or (data and data:match(p)) or (saveddata and saveddata:match(p))
 
-  --[[ fix the code below, and resort to it, if patterns fail totally
-	local match,mfrom,mto
-	if not id then --duh, just looks better
-		match,mfrom,mto = zo_plainstrfind(comdata,name.."=")
-		if match then
-			id=tonumber(comdata:match("^=(%d+)",mto))
-			--assert(id,"what, no id? found "..name.." in comdata at "..mfrom
+	local SetMapToPlayerLocation, ZO_WorldMap_UpdateMap, GetCurrentMapZoneIndex = _G.SetMapToPlayerLocation, _G.ZO_WorldMap_UpdateMap, _G.GetCurrentMapZoneIndex
+
+	if not id then
+		if not ZGV.DEV then
+			id = 6660000+lamehash(name)   -- fake hashy ID
+		else
+			-- Create a new id for it. and stow it in SV
+			SetMapToPlayerLocation()
+			ZO_WorldMap_UpdateMap()
+			local zone = abs(GetCurrentMapZoneIndex())
+			if zone>999 then zone=999 end
+
+			local zoneid = ("%03d"):format(zone)
+			local fac = faction_to_num()
+			local zone_fac = zoneid..fac
+
+			-- increment the current count.
+			lastIds[zone_fac] = (lastIds[zone_fac] or 0) + 1
+
+			id = zone_fac..("%03d"):format(lastIds[zone_fac])
+
+			local newentry = ("%s=%s"):format(name,id)
+			appendSavedVarsByType(typ,newentry)
 		end
 	end
-	if not id then
-		match,mfrom,mto = zo_plainstrfind(data,name.."=")
-		if match then id=tonumber(data:match("^=(%d+)",mto)) end
-	end
-	if not id then
-		match,mfrom,mto = zo_plainstrfind(saveddata,name.."=")
-		if match then id=tonumber(saveddata:match("^=(%d+)",mto)) end
-	end
-	--]]
 
-
-  if not id then
-    if not ZGV.DEV then
-      id = 6660000+lamehash(name)   -- fake hashy ID
-    else
-      -- Create a new id for it. and stow it in SV
-      SetMapToPlayerLocation()
-      ZO_WorldMap_UpdateMap()
-      local zone = abs(GetCurrentMapZoneIndex())
-      if zone>999 then zone=999 end
-      local zoneid = ("%03d"):format(zone)
-
-      local fac = faction_to_num()
-
-      local zone_fac = zoneid..fac
-
-      -- increment the current count.
-      lastIds[zone_fac] = (lastIds[zone_fac] or 0) + 1
-
-      id = zone_fac..("%03d"):format(lastIds[zone_fac])
-
-      local newentry = ("%s=%s"):format(name,id)
-      appendSavedVarsByType(typ,newentry)
-    end
-  end
-
-  return tonumber(id)
+	return tonumber(id)
 end
 
 function Data:GetNameByIdAndType(typ,id)
-  if not (typ and id) then return end
-  local comdata,data,saveddata = getCorrectVarsByType(typ)
+	if not (typ and id) then return end
+	local comdata,data,saveddata = getCorrectVarsByType(typ)
 
-  id = ("%07d"):format(abs(tonumber(id)))	-- Make sure we have as many leading 0s as needed
+	id = ("%07d"):format(abs(tonumber(id)))	-- Make sure we have as many leading 0s as needed
 
-  local p = '([^=\r\n]+)='..id	-- A series of non-special chars, then =, then ID.
+	local p = '([^=\r\n]+)='..id	-- A series of non-special chars, then =, then ID.
 
-  local name = comdata:match(p) or data:match(p) or saveddata:match(p)
-  --local name1 = comdata:match(p)
-  --local name2 = data:match(p)
-  --local name3 = saveddata:match(p)
-  --name = name1 or name2 or name3
+	local name = comdata:match(p) or data:match(p) or saveddata:match(p)
 
-  if not name then
-    -- Might just be a different faction
-    self:Debug("No name found for type:%s, id:%s",typ,id)
-  end
+	if not name then
+		-- Might just be a different faction
+		self:Debug("No name found for type:%s, id:%s",typ,id)
+	end
 
-  return name
+	return name
 end
 
 -- Update the last used id from our data and saved vars so we know what to assign next
 function Data:UpdateLastIds(typ)
-  local comdata,data,saveddata,lastIds = getCorrectVarsByType(typ)
+	local comdata,data,saveddata,lastIds = getCorrectVarsByType(typ)
 
-  local p = '[^=]+=(%d+)\n?'
+	local p = '[^=]+=(%d+)\n?'
 
-  for i,loopdata in ipairs({comdata,data,saveddata}) do
-    local s,e = loopdata:find(p)
+	for i,loopdata in ipairs({comdata,data,saveddata}) do
+		local s,e = loopdata:find(p)
 
-    while( e ) do
-      -- Get the id and then split it.
-      local id = loopdata:match(p,s)
-      local zoneid_fac,num = id:match("(%d%d%d%d)(%d+)")
-      num = tonumber(num)
+		while( e ) do
+			-- Get the id and then split it.
+			local id = loopdata:match(p,s)
+			local zoneid_fac,num = id:match("(%d%d%d%d)(%d+)")
+			num = tonumber(num)
 
-      -- Ids are all incremental so the highest one is the correct one.
-      if not lastIds[zoneid_fac] then lastIds[zoneid_fac] = 0 end
+			-- Ids are all incremental so the highest one is the correct one.
+			if not lastIds[zoneid_fac] then lastIds[zoneid_fac] = 0 end
 
-      if num > lastIds[zoneid_fac] then lastIds[zoneid_fac] = num end
+			if num > lastIds[zoneid_fac] then lastIds[zoneid_fac] = num end
 
-      s,e = loopdata:find(p,e)
-    end
-  end
+			s,e = loopdata:find(p,e)
+		end
+	end
 end
 
 -----------------------------------------
@@ -238,19 +202,19 @@ end
 -----------------------------------------
 
 function Data:GetQuestIdByName(qname)
-  local id = self:GetIdByNameAndType(QUEST_STR,qname)
+	local id = self:GetIdByNameAndType(QUEST_STR,qname)
 
-  return id
+	return id
 end
 
 function Data:GetQuestNameById(qid)
-  local name = self:GetNameByIdAndType(QUEST_STR,qid)
+	local name = self:GetNameByIdAndType(QUEST_STR,qid)
 
-  return name or ("(quest %d)"):format(qid or 0)
+	return name or ("(quest %d)"):format(qid or 0)
 end
 
 function Data:UpdateLastQuestIds()
-  self:UpdateLastIds(QUEST_STR)
+	self:UpdateLastIds(QUEST_STR)
 end
 
 -----------------------------------------
@@ -259,9 +223,9 @@ end
 
 -- Common data > alliance data > data in SV
 function Data:GetQuestStepDataByQuestId(qid)
-  local comdata,data = getCorrectVarsByType(QUEST_STEP_STR)
+	local comdata,data = getCorrectVarsByType(QUEST_STEP_STR)
 
-  return (comdata and comdata[qid]) or (data and data[qid])
+	return (comdata and comdata[qid]) or (data and data[qid])
 end
 
 -----------------------------------------
@@ -270,19 +234,19 @@ end
 
 -- Make sure the name passed is an npc before getting passed.
 function Data:GetNpcIdByName(nname)
-  local id = self:GetIdByNameAndType(NPC_STR,nname)
+	local id = self:GetIdByNameAndType(NPC_STR,nname)
 
-  return id
+	return id
 end
 
 function Data:GetNpcNameById(nid)
-  local name = self:GetNameByIdAndType(NPC_STR,nid)
+	local name = self:GetNameByIdAndType(NPC_STR,nid)
 
-  return name or ("(name %d)"):format(nid or 0)
+	return name or ("(name %d)"):format(nid or 0)
 end
 
 function Data:UpdateLastNpcIds()
-  self:UpdateLastIds(NPC_STR)
+	self:UpdateLastIds(NPC_STR)
 end
 
 -----------------------------------------
@@ -291,19 +255,19 @@ end
 
 -- Make sure the name passed is an npc before getting passed.
 function Data:GetObjectIdByName(nname)
-  local id = self:GetIdByNameAndType(OBJECT_STR,nname)
+	local id = self:GetIdByNameAndType(OBJECT_STR,nname)
 
-  return id
+	return id
 end
 
 function Data:GetObjectNameById(oid)
-  local name = self:GetNameByIdAndType(OBJECT_STR,oid)
+	local name = self:GetNameByIdAndType(OBJECT_STR,oid)
 
-  return name or ("(object %d)"):format(oid or 0)
+	return name or ("(object %d)"):format(oid or 0)
 end
 
 function Data:UpdateLastObjectIds()
-  self:UpdateLastIds(OBJECT_STR)
+	self:UpdateLastIds(OBJECT_STR)
 end
 
 -----------------------------------------
@@ -311,8 +275,8 @@ end
 -----------------------------------------
 
 function Data:Debug(...)
-  local str = ...
-  ZGV:Debug("&data "..str, select(2,...) )
+	local str = ...
+	ZGV:Debug("&data "..str, select(2,...) )
 end
 
 -----------------------------------------
@@ -320,70 +284,70 @@ end
 -----------------------------------------
 
 tinsert(ZGV.startups,function(self)
-    svdata = ZGV.sv.profile.data
+		svdata = ZGV.sv.profile.data
 
+		-- common: store directly
+		npcDataCom = ZGV._NpcDataCommon					ZGV._NpcDataCommon = nil
+		questDataCom = ZGV._QuestDataCommon				ZGV._QuestDataCommon = nil
+		questStepDataCom = ZGV._QuestStepDataCommon  	ZGV._QuestStepDataCommon = nil
+		objectDataCom = ZGV._ObjectDataCommon  			ZGV._ObjectDataCommon = nil
 
-    -- common: store directly
+		local fac = ZGV.Utils.GetFaction()
+		local datatypes={"_QuestStepData","_QuestData","_NpcData","_ObjectData","_ItemData"}
 
-    npcDataCom = ZGV._NpcDataCommon  ZGV._NpcDataCommon=nil
-    questDataCom = ZGV._QuestDataCommon  ZGV._QuestDataCommon=nil
-    questStepDataCom = ZGV._QuestStepDataCommon  ZGV._QuestStepDataCommon=nil
-    objectDataCom = ZGV._ObjectDataCommon  ZGV._ObjectDataCommon=nil
+		-- old style, temporary
+		for v in pairs(datatypes) do 
+			if ZGV[v] then 
+				ZGV[v..fac]=ZGV[v] ZGV[v] = nil
+			end 
+		end
 
+		-- faction data: store directly
+		questStepData = ZGV['_QuestStepData'..fac] or ""  ZGV['_QuestStepData'..fac]=nil
+		npcData = ZGV['_NpcData'..fac] or ""  ZGV['_NpcData'..fac]=nil
+		objectData = ZGV['_ObjectData'..fac] or ""  ZGV['_ObjectData'..fac]=nil
+		questData = ZGV['_QuestData'..fac] or ""  ZGV['_QuestData'..fac]=nil
+		itemData = ZGV['_ItemData'..fac] or ""  ZGV['_ItemData'..fac]=nil
 
-    local fac = ZGV.Utils.GetFaction()
+		assert(questData,"WHOA! We have NO quests for faction "..fac.."!!")
 
-    --table.insert(ZGV.PRELOG,"Data Faction: "..fac)
+		local GetUnitLevel = _G.GetUnitLevel
+		-- weed out wrong-faction data
+		if GetUnitLevel("player")<48 then
+			-- surely no veteran content, trash all factions now
+			for k in pairs(datatypes) do 
+				for fa in ipairs{'AD','DC','EP'} do 
+					ZGV[k..fa]=nil 
+				end 
+			end
+		end
 
-    local datatypes={"_QuestStepData","_QuestData","_NpcData","_ObjectData","_ItemData"}
+		-- add other factions to this one (if there are any left, after the massacre above.)
+		for fa in ipairs{'AD','DC','EP'} do 
+			if fa ~= fac then
+				npcData = npcData .. 		(ZGV['_NpcData'..fa] or "")  		ZGV['_NpcData'..fa] = nil
+				objectData = objectData .. 	(ZGV['_ObjectData'..fa] or "")  	ZGV['_ObjectData'..fa] = nil
+				questData = questData .. 	(ZGV['_QuestData'..fa] or "")  		ZGV['_QuestData'..fa] = nil
+				itemData = itemData .. 		(ZGV['_ItemDataAD'..fa] or "")  	ZGV['_ItemDataAD'..fa] = nil
+				ZGV.Utils.table_join(questStepData,ZGV['_QuestStepData'..fa])  	ZGV['_QuestStepData'..fa] = nil
+			end 
+		end
 
+		-- All these references aren't needed. Just for testing
+		Data.npcDataCom = npcDataCom
+		Data.questDataCom = questDataCom
+		Data.questStepDataCom = questStepDataCom
+		Data.objectDataCom = objectDataCom
+		Data.npcData = npcData
+		Data.questData = questData
+		Data.questStepData = questStepData
+		Data.objectData = objectData
+		Data.itemData = itemData
 
-    -- old style, temporary
-    for k,v in pairs(datatypes) do if ZGV[v] then ZGV[v..fac]=ZGV[v] ZGV[v]=nil end end
+		collectgarbage()
 
+		Data:UpdateLastQuestIds()
+		Data:UpdateLastNpcIds()
+		Data:UpdateLastObjectIds()
 
-    -- faction data: store directly
-
-    questStepData = ZGV['_QuestStepData'..fac] or ""  ZGV['_QuestStepData'..fac]=nil
-    npcData = ZGV['_NpcData'..fac] or ""  ZGV['_NpcData'..fac]=nil
-    objectData = ZGV['_ObjectData'..fac] or ""  ZGV['_ObjectData'..fac]=nil
-    questData = ZGV['_QuestData'..fac] or ""  ZGV['_QuestData'..fac]=nil
-    itemData = ZGV['_ItemData'..fac] or ""  ZGV['_ItemData'..fac]=nil
-
-    assert(questData,"WHOA! We have NO quests for faction "..fac.."!!")
-
-    -- weed out wrong-faction data
-    if GetUnitLevel("player")<48 then
-      -- surely no veteran content, trash all factions now
-      for k,v in pairs(datatypes) do for fi,fa in ipairs{'AD','DC','EP'} do ZGV[k..fa]=nil end end
-    end
-
-    -- add other factions to this one (if there are any left, after the massacre above.)
-
-    for fi,fa in ipairs{'AD','DC','EP'} do if fa~=fac then
-      npcData = npcData .. (ZGV['_NpcData'..fa] or "")  ZGV['_NpcData'..fa]=nil
-      objectData = objectData .. (ZGV['_ObjectData'..fa] or "")  ZGV['_ObjectData'..fa]=nil
-      questData = questData .. (ZGV['_QuestData'..fa] or "")  ZGV['_QuestData'..fa]=nil
-      itemData = itemData .. (ZGV['_ItemDataAD'..fa] or "")  ZGV['_ItemDataAD'..fa]=nil
-      ZGV.Utils.table_join(questStepData,ZGV['_QuestStepData'..fa])  ZGV['_QuestStepData'..fa]=nil
-    end end
-
-    -- All these references aren't needed. Just for testing
-
-    Data.npcDataCom = npcDataCom
-    Data.questDataCom = questDataCom
-    Data.questStepDataCom = questStepDataCom
-    Data.objectDataCom = objectDataCom
-    Data.npcData = npcData
-    Data.questData = questData
-    Data.questStepData = questStepData
-    Data.objectData = objectData
-    Data.itemData = itemData
-
-    collectgarbage()
-
-    Data:UpdateLastQuestIds()
-    Data:UpdateLastNpcIds()
-    Data:UpdateLastObjectIds()
-
-  end)
+	end)
--- Events.lua
+++ Events.lua
@@ -1,5 +1,4 @@
-local ZGV = ZygorGuidesViewer
-if not ZGV then return end
+local ZGV = _G.ZGV
 
 --[[
 	:AddEvent(EVENT,function() end)		- > Calls function
@@ -12,17 +11,15 @@ if not ZGV then return end
 -- LOCAL REFERENCES
 -----------------------------------------
 
-local tinsert,tremove,sort,min,max,floor,type,pairs,ipairs = table.insert,table.remove,table.sort,math.min,math.max,math.floor,type,pairs,ipairs
-local print = ZGV.print
-local CHAIN = ZGV.Utils.ChainCall
-local ui = ZGV.UI
+local tinsert, tremove, type, pairs, ipairs = table.insert, table.remove, type, pairs, ipairs
 
 -----------------------------------------
 -- LOCAL VARIABLES
 -----------------------------------------
 
 local Events = {}
-local eventFrame = ZGV.WM:CreateControl("ZGV_EventFrame",GuiRoot,CT_TOPLEVELCONTROL)
+local GuiRoot, uiTopLevelControl = _G.GuiRoot, _G.CT_TOPLEVELCONTROL
+local eventFrame = ZGV.WM:CreateControl("ZGV_EventFrame",GuiRoot,uiTopLevelControl)
 
 -----------------------------------------
 -- SAVED REFERENCES
@@ -35,54 +32,61 @@ ZGV.Events = Events
 -----------------------------------------
 
 local function EventHandler(event,...)
-  local self = Events
-  local eventString = self.eventList[event]
-  assert(eventString,"Event "..event.." is not in our eventlist!")
+	local self = Events
+	local eventString = self.eventList[event]
+	assert(eventString,"Event "..event.." is not in our eventlist!")
 
-  for i,hand in ipairs(self[event]) do
-    local func
-    local funcSelf
-    if type(hand)=="function" then
-      func=hand
-      funcSelf = self
-    elseif type(hand)=="string" then
-      func=self[hand] or ZGV[hand]
-      funcSelf = self[hand] and self or ZGV
-      assert(func,"No function "..hand.." in event handler!")
-    elseif type(hand)=="table" then
-      func=hand[eventString]
-      funcSelf = hand
-      assert(func,"No function "..eventString.." in provided table!")
-    elseif hand==true then
-      func=self[eventString] or ZGV[eventString]
-      funcSelf = self[eventString] and self or ZGV
-      assert(func,"No function "..eventString.." in event handler!")
-    end
-    func(funcSelf,event,...)	
-  end
+	for i, hand in ipairs(self[event]) do
+		local func
+		local funcSelf
+		if type(hand) == "function" then
+			func = hand
+			funcSelf = self
+		elseif type(hand) == "string" then
+			func = self[hand] or ZGV[hand]
+			funcSelf = self[hand] and self or ZGV
+			assert(func,"No function "..hand.." in event handler!")
+		elseif type(hand) == "table" then
+			func = hand[eventString]
+			funcSelf = hand
+			assert(func,"No function "..eventString.." in provided table!")
+		elseif hand == true then
+			func = self[eventString] or ZGV[eventString]
+			funcSelf = self[eventString] and self or ZGV
+			assert(func,"No function "..eventString.." in event handler!")
+		end
+		func(funcSelf,event,...)
+	end
 end
+
 
 -----------------------------------------
 -- FUNCTIONS
 -----------------------------------------
 
 function Events:AddEvent(event,func)
-  if not self[event] then self[event] = {} end
+	if not self[event] then 
+		self[event] = {} 
+	end
 
-  tinsert(self[event],func or true)
-  if #self[event]==1 then eventFrame:RegisterForEvent(event,EventHandler) end
+	tinsert(self[event],func or true)
+	if #self[event] == 1 then 
+		eventFrame:RegisterForEvent(event,EventHandler) 
+	end
 end
 
 function Events:RemoveEvent(event,removefunc)
-  if not self[event] then return end
+	if not self[event] then return end
 
-  for num,func in ipairs(self[event]) do
-    if func == removefunc then
-      tremove(self[event],num)
+	for num,func in ipairs(self[event]) do
+		if func == removefunc then
+			tremove(self[event],num)
 
-      if #self[event]==0 then eventFrame:UnregisterForEvent(event) end
-    end
-  end
+			if #self[event]==0 then 
+				eventFrame:UnregisterForEvent(event) 
+			end
+		end
+	end
 end
 
 -----------------------------------------
@@ -90,15 +94,15 @@ end
 -----------------------------------------
 -- state = true -> Enter Combat
 -- state = false -> Exit Combat
-function ZGV:EVENT_PLAYER_COMBAT_STATE(event,state)
-  if not state then
+local state = _G.state
+function ZGV:EVENT_PLAYER_COMBAT_STATE(state)
+	if not state then
+		if self.call_after_combat then
+			self.call_after_combat()
+			self.call_after_combat = nil
+		end
 
-    if self.call_after_combat then
-      self.call_after_combat()
-      self.call_after_combat = nil
-    end
-
-  end
+	end
 end
 
 -----------------------------------------
@@ -106,57 +110,67 @@ end
 -----------------------------------------
 
 tinsert(ZGV.startups,function(self)
-    Events:AddEvent(EVENT_PLAYER_COMBAT_STATE)
-  end)
+		local EVENT_PLAYER_COMBAT_STATE = _G.EVENT_PLAYER_COMBAT_STATE
+		Events:AddEvent(EVENT_PLAYER_COMBAT_STATE)
+	end)
 
 -----------------------------------------
 -- EVENT LIST  - now dynamic.
 -----------------------------------------
 local safenext = function(table,index)
-  local ok,k,v = pcall(next,table,index)
-  if ok then
-    return k,v
-  else
-    -- when pcall fails, it gives an error message. The failing index will be there!
-    local newk = k:match(" function '(.-)'")
-    if newk then
-      return newk,"PROT/PRIV"
-    else
-      -- sad failure
-    end
-  end  -- k has the error message, important
-end
-
-local safepairs = function(table)
-  return safenext,table,nil
+	local ok, k, v = pcall(next,table,index)
+	if ok then
+		return k, v
+	else
+		-- when pcall fails, it gives an error message. The failing index will be there!
+		local newk = k:match(" function '(.-)'")
+		if newk then
+			return newk,"PROT/PRIV"
+		else
+			-- sad failure
+		end
+	end  -- k has the error message, important
 end
 
 local globalprefixes = function(prefix)
-  local safeglobalnext = function(tab,index)
-    local val
-    local safety=0
-    repeat
-      index,val = safenext(_G,index)
-      if index and index:find("^"..prefix) then return index,val end
-      safety=safety+1  if safety>20000 then return "ERR","ERR" end
-    until not index
-  end
-  return safeglobalnext,_G,nil
+	local safeglobalnext = function(tab,index)
+		local val
+		local safety=0
+		repeat
+			index,val = safenext(_G,index)
+			if index and index:find("^"..prefix) then 
+				return index,val 
+			end
+			safety=safety + 1
+			if safety > 20000 then 
+				return "ERR","ERR" 
+			end
+		until not index
+	end
+	return safeglobalnext,_G,nil
 end
 
 local getglobalbyprefix = function(prefix,value)
-  for k,v in globalprefixes(prefix) do if v==value then return k end end
+	for k, v in globalprefixes(prefix) do 
+		if v == value then 
+			return k 
+		end 
+	end
 end
 
 Events.eventList = {}
-for k,v in globalprefixes("EVENT_") do
-  if type(v)=="number"
-  and k~="EVENT_GLOBAL_MOUSE_DOWN" 
-  and k~="EVENT_GLOBAL_MOUSE_UP" 
-  then Events.eventList[v]=k
-  end
+for k, v in globalprefixes("EVENT_") do
+	if type(v) == "number"
+	and k ~= "EVENT_GLOBAL_MOUSE_DOWN" 
+	and k ~= "EVENT_GLOBAL_MOUSE_UP" 
+	then 
+		Events.eventList[v] = k
+	end
 end
 
 setmetatable(Events.eventList,{__index = "NO EVENT IN LIST!?!?"})
 
-Events.eventListR = {}  for k,v in pairs(Events.eventList) do Events.eventListR[v]=k end
+Events.eventListR = {}
+for k, v in pairs(Events.eventList) do 
+	Events.eventListR[v]=k 
+end
