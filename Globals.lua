0a0c9a8a94f51c2835b50c7453bdec28bda185a0
Hydra9268 <kxmode1@gmail.com>
2018-12-22 00:37:58 -0800

    Removed unnecessary local variable

--- Globals.lua
+++ Globals.lua
@@ -1,10 +1,3 @@
-local ZGV = ZygorGuidesViewer
-if not ZGV then return end
-
------------------------------------------
--- LOCAL REFERENCES
------------------------------------------
-
 EMPTY_STRING = ""
 LEFT_MOUSE_BUTTON = 1
 RIGHT_MOUSE_BUTTON = 2
