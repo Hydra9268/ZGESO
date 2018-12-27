4655faca870471f74dd5d37dc9db6cab2dda0756
Hydra9268 <kxmode1@gmail.com>
2018-12-27 04:31:51 -0800

    Indent adjustments;

--- Localization/Main.lua
+++ Localization/Main.lua
@@ -9,7 +9,7 @@ Core_xxXX files provide full UI texts, even in English. Just to have them all in
 -- LOCAL REFERENCES
 -----------------------------------------
 
-local tinsert, tremove, sort, min, max, floor, type, pairs, ipairs, class = table.insert, table.remove, table.sort, math.min, math.max, math.floor, type, pairs, ipairs, class
+local tinsert, tremove, sort, min, max, floor, type, pairs, ipairs = table.insert, table.remove, table.sort, math.min, math.max, math.floor, type, pairs, ipairs
 
 -----------------------------------------
 -- LOCAL VARIABLES
@@ -18,36 +18,36 @@ local tinsert, tremove, sort, min, max, floor, type, pairs, ipairs, class = tabl
 local data = {}
 
 local mt = {__index = function(self, key)
-    if type(key) == "number" then return end
-    self[key] = key
-    return key
-  end}
+		if type(key) == "number" then return end
+		self[key] = key
+		return key
+	end}
 
 -----------------------------------------
 -- FUNCTIONS
 -----------------------------------------
 
 function GuideViewer(name, locale, translations)
-  if type(translations) == "function" then translations = translations() end
-  if type(translations) == "string" then
-    -- oh crap, parse the monster.
-    local arr = {}
-    local function grab(txt)
-      local k, v = txt:gsub("%s*//.*$", ""):match("^%s*(.-)%s*===%s*(.-)%s*$")
-      if k and v then arr[k] = v end
-    end
-    translations:gsub("(.-)\n", grab)
-    translations = arr
-  end
+	if type(translations) == "function" then translations = translations() end
+	if type(translations) == "string" then
+		-- oh crap, parse the monster.
+		local arr = {}
+		local function grab(txt)
+			local k, v = txt:gsub("%s*//.*$", ""):match("^%s*(.-)%s*===%s*(.-)%s*$")
+			if k and v then arr[k] = v end
+		end
+		translations:gsub("(.-)\n", grab)
+		translations = arr
+	end
 
-  if not data[name] then
-    data[name] = {}
-  end
-  setmetatable(data[name], mt)
+	if not data[name] then
+		data[name] = {}
+	end
+	setmetatable(data[name], mt)
 
-  if locale then
-    data[name] = translations
-  end
+	if locale then
+		data[name] = translations
+	end
 
-  return data[name]
+	return data[name]
 end
