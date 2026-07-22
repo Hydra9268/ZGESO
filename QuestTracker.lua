07864410cfb9ef5a940187b3848800d4d0b89e0f
Ryan Allen <Hydra9268@users.noreply.github.com>
2026-07-21 23:50:10 -0700

    Optimize QuestTracker performance and clean up event handlers

--- QuestTracker.lua
+++ QuestTracker.lua
@@ -3,11 +3,19 @@
 -----------------------------------------
 
 local CGV = _G.CGV
-local tinsert,tremove,sort,min,max,floor,type,pairs,ipairs = table.insert,table.remove,table.sort,math.min,math.max,math.floor,type,pairs,ipairs
-local print = CGV.print
+
+local tinsert = table.insert
+local tostring = tostring
+
+local GetFrameTimeMilliseconds = _G.GetFrameTimeMilliseconds
+local GetNextCompletedQuestId = _G.GetNextCompletedQuestId
+local GetCompletedQuestInfo = _G.GetCompletedQuestInfo
+local RequestJournalQuestConditionAssistance = _G.RequestJournalQuestConditionAssistance
+local zo_callLater = _G.zo_callLater
+
 local CHAIN = CGV.Utils.ChainCall
-local ui = CGV.UI
 local d = _G.d
+
 local QuestTracker = {}
 
 QuestTracker.questpositionrequests = {}
@@ -24,6 +32,25 @@ local EVENT_QUEST_LIST_UPDATED = _G.EVENT_QUEST_LIST_UPDATED
 -----------------------------------------
 
 CGV.QuestTracker = QuestTracker
+
+-----------------------------------------
+-- LOCAL FUNCTIONS
+-----------------------------------------
+
+local pendingQuestUpdates = {}
+
+local function QueueQuestUpdate(journalIndex)
+	if pendingQuestUpdates[journalIndex] then
+		return
+	end
+
+	pendingQuestUpdates[journalIndex] = true
+
+	zo_callLater(function()
+		pendingQuestUpdates[journalIndex] = nil
+		CGV.Quests:UpdateQuest(journalIndex)
+	end,0)
+end
 
 -----------------------------------------
 -- FUNCTIONS
@@ -44,45 +71,57 @@ function QuestTracker:GatherAllCurrentQuests()
 end
 
 QuestTracker.CompletedQuests = {}
+
 local ever_cached = false
-local last_CCQ = 0
-function QuestTracker:CacheCompletedQuests()  -- t=1ms for 300+ quests
+local lastCacheTime
+
+function QuestTracker:CacheCompletedQuests()
+	local frameTime = GetFrameTimeMilliseconds()
+
+	if frameTime == lastCacheTime then
+		return self.CompletedQuests
+	end
+
+	lastCacheTime = frameTime
+
+	local completedQuests = self.CompletedQuests
+	CGV.Utils.table_wipe_keys(completedQuests)
+
 	local id
 	local count = 0
 
-	-- Do not run twice per frame. It makes no sense, really, even if it's 1ms fast.
-	local t = _G.GetFrameTimeMilliseconds()
-	if t == last_CCQ then
-		return self.CompletedQuests
-	else
-		last_CCQ = t
-	end
-
-	CGV.Utils.table_wipe_keys(self.CompletedQuests)
 	repeat
-		id = _G.GetNextCompletedQuestId(id)
+		id = GetNextCompletedQuestId(id)
+
 		if id then
-			local title = _G.GetCompletedQuestInfo(id)
-			self.CompletedQuests[id]=title
-			if (self.CompletedQuests[title] or title) ~= title then
-				title = "DUPE: "..title
+			local title = GetCompletedQuestInfo(id)
+
+			completedQuests[id] = title
+
+			if completedQuests[title] then
+				completedQuests["DUPE: " .. title] = id
+			else
+				completedQuests[title] = id
 			end
-			self.CompletedQuests[title] = id
 		end
+
 		count = count + 1
+
 		if count > 1000000 then
 			return false
 		end
 	until not id
-	local ever_cached = true
 
-	return self.CompletedQuests
+	ever_cached = true
+
+	return completedQuests
 end
 
 function QuestTracker:IsQuestComplete(id_or_title)
 	if not ever_cached then
 		self:CacheCompletedQuests()
 	end
+
 	return self.CompletedQuests[id_or_title]
 end
 
@@ -90,74 +129,85 @@ end
 -- EVENT FUNCTIONS
 -----------------------------------------
 
-function QuestTracker:EVENT_QUEST_ADDED(event,journalIndex,questName,objectiveName)
+function QuestTracker:EVENT_QUEST_ADDED(_, journalIndex, _, _)
 	-- New Quest! Lets assume this is the first step in a quest.
-	CGV.Quests:GetQuest(journalIndex)	-- Get the Quest just so it gets loaded into CGV.Quests
+	CGV.Quests:GetQuest(journalIndex)
 end
 
-function QuestTracker:EVENT_QUEST_REMOVED(event,isCompleted,journalIndex,questName,zoneIndex,poiIndex)
+function QuestTracker:EVENT_QUEST_REMOVED(_, isCompleted, journalIndex, _, _, _)
 	if CGV.DEV then
-		d("QUEST REMOVED! journal ".. journalIndex .." isC ".. tostring(isCompleted))
+		d("QUEST REMOVED! journal " .. journalIndex .. " isC " .. tostring(isCompleted))
 	end
+
 	self:CacheCompletedQuests()
 	CGV.Quests:RemoveQuest(journalIndex)
 end
 
--- Fired to update the quest to the next step and thus new conditions
-function QuestTracker:EVENT_QUEST_ADVANCED(event,journalIndex,questName,isPushed,isComplete,mainStepChanged)
-	CGV.Quests:UpdateQuest(journalIndex)
+-- Fired to update the quest to the next step and thus new conditions.
+function QuestTracker:EVENT_QUEST_ADVANCED(_, journalIndex, _, _, isComplete, _)
+	QueueQuestUpdate(journalIndex)
+
 	--if isComplete then CGV.Quests:MarkQuestCompletion(journalIndex,true) end
 	-- Actually, NOT TRUE! A quest is announced as complete here even if it's on the LAST STAGE now!!!
 end
 
--- Fired before EVENT_QUEST_ADVANCED to update the previous condition to complete
-function QuestTracker:EVENT_QUEST_CONDITION_COUNTER_CHANGED(	event,
-																journalIndex,
-																questName,
-																conditionText,
-																conditionType,
-																currConditionVal,
-																newConditionVal,
-																conditionMax,
-																isFailCondition,
-																stepOverrideText,
-																isPushed,
-																isComplete,
-																isConditionComplete,
-																isStepHidden)
-	CGV.Quests:UpdateQuest(journalIndex) -- oh just update it all
+-- Fired before EVENT_QUEST_ADVANCED to update the previous condition to complete.
+function QuestTracker:EVENT_QUEST_CONDITION_COUNTER_CHANGED(_, journalIndex, _, _, _, _, _, _, _, _, _, _, _, _)
+	QueueQuestUpdate(journalIndex)
 end
 
 function QuestTracker:EVENT_QUEST_LIST_UPDATED()
 	self:GatherAllCurrentQuests()
 end
 
-function QuestTracker:EVENT_QUEST_POSITION_REQUEST_COMPLETE(event,requestid,typ,x,y,r,wtf1,wtf2)
+function QuestTracker:EVENT_QUEST_POSITION_REQUEST_COMPLETE(_, requestid, typ, x, y, r, wtf1, wtf2)
 	local request = self.questpositionrequests[requestid]
-	if not request then return end
+	if not request then
+		return
+	end
+
 	CGV.Quests:SetConditionCoords(
-		request.journalIndex, request.stepnum, request.condnum,
-		typ,CGV.Pointer:GetMapTex(),x,y,r,wtf1,wtf2
+		request.journalIndex,
+		request.stepnum,
+		request.condnum,
+		typ,
+		CGV.Pointer:GetMapTex(),
+		x,
+		y,
+		r,
+		wtf1,
+		wtf2
 	)
-	self.questpositionrequests[requestid]=nil
+
+	self.questpositionrequests[requestid] = nil
 end
 
 -----------------------------------------
 -- STARTUP
 -----------------------------------------
-QuestTracker:CacheCompletedQuests()		-- quest completion is working already! at load time! woo!
-CGV.Utils.CheckVeteranFaction()  		-- IMMEDIATELY. Because... we can!
+
+QuestTracker:CacheCompletedQuests()	-- quest completion is working already! at load time! woo!
+CGV.Utils.CheckVeteranFaction()	-- IMMEDIATELY. Because... we can!
 
 tinsert(CGV.startups,function(self)
-		QuestTracker:RegisterEvents()
-		QuestTracker:GatherAllCurrentQuests()
-	end)
+	QuestTracker:RegisterEvents()
+	QuestTracker:GatherAllCurrentQuests()
+end)
 
+local OriginalRequestJournalQuestConditionAssistance = RequestJournalQuestConditionAssistance
 
-local _RequestJournalQuestConditionAssistance = RequestJournalQuestConditionAssistance
-function RequestJournalQuestConditionAssistance(journalIndex,stepnum,condnum,boo,baa)
-	local reqid = _RequestJournalQuestConditionAssistance(journalIndex,stepnum,condnum,boo)
-	if not reqid then return end
-	QuestTracker.questpositionrequests[reqid] = { journalIndex = journalIndex, stepnum = stepnum, condnum = condnum }
-	return reqid
+function RequestJournalQuestConditionAssistance(journalIndex, stepnum, condnum, boo, baa)
+	local requestId = OriginalRequestJournalQuestConditionAssistance(journalIndex, stepnum, condnum, boo)
+
+	if not requestId then
+		return
+	end
+
+	QuestTracker.questpositionrequests[requestId] = {
+		journalIndex = journalIndex,
+		stepnum = stepnum,
+		condnum = condnum,
+	}
+
+	return requestId
 end
